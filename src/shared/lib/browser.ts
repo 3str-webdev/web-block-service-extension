3494ee3b8e9a8f3fec22384eaf5829fa9fd3f33e
3str-webdev <3str.webdev@gmail.com>
2023-11-01 11:07:32 +0300

    feat: add browser extention implements for setInterval and helper for set net rules

--- src/shared/lib/browser.ts
+++ src/shared/lib/browser.ts
@@ -1,3 +1,44 @@
 export const createTab = (url: string) => {
   chrome.tabs.create({ url });
 };
+
+export type NetRule = chrome.declarativeNetRequest.Rule;
+export const NetRuleActionType = chrome.declarativeNetRequest.RuleActionType;
+export const NetRuleResourceType = chrome.declarativeNetRequest.ResourceType;
+
+export const setNetRules = async (newRules: NetRule[]) => {
+  const oldRules = await chrome.declarativeNetRequest.getDynamicRules();
+  const oldRuleIds = oldRules.map((rule) => rule.id);
+
+  await chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: oldRuleIds,
+    addRules: newRules,
+  });
+};
+
+export const setBrowserInterval = async (
+  name: string,
+  cb: () => void,
+  timeout: number,
+) => {
+  await chrome.alarms.create(name, {
+    delayInMinutes: timeout / 60 / 1000,
+    periodInMinutes: timeout / 60 / 1000,
+  });
+
+  chrome.alarms.onAlarm.addListener((alarm) => {
+    if (name === alarm.name) {
+      cb();
+    }
+  });
+};
+
+export const addInstallListener = (cb: () => Awaited<void>) => {
+  chrome.runtime.onInstalled.addListener(async ({ reason }) => {
+    if (reason !== "install") {
+      return;
+    }
+
+    cb();
+  });
+};
